232429344ce171c091edc34609879bfd606d5c90
Vikas Gavandi <vekgraphics@gmail.com>
2021-08-27 10:13:29 +0530

    Deploy
    Deploy new code of project

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+			  question: "21.	Which of the following is true statement?",
+			  option1: "Evidence has shown that Levosalbutamol is not cost-effective treatment when compared to racemic salbutamol",
+			  option2: "Evidence has shown that Levosalbutamol is more cost-effective treatment when compared to racemic salbutamol",
+			  option3: "Both are equally cost effective",
+			  option4: "None of the above",
+			  answer: 2
+			},
+			{
+			  question: "What is the major route of excretion of levosalbutamol",
+			  option1: "Liver",
+			  option2: "Lungs",
+			  option3: "Kidneys",
+			  option4: "Sweat",
+			  answer: 3
+			},
+			{
+			  question: "Which of the following statement is true regarding ambroxol?",
+			  option1: "There are many absolute contraindications for Ambroxol, but no caution is required in patients with gastric ulceration",
+			  option2: "Gastric ulceration is only absolute contraindication for ambroxol.",
+			  option3: "There are not many absolute contraindications for Ambroxol, but in patients with gastric ulceration, relative caution should be observed",
+			  option4: "None of the above",
+			  answer: 3
+			},
+			{
+				question: "Guaiphenesin was first accepted by the USFDA",
+				option1: "As early as 1952",
+				option2: "As early as 1925",
+				option3: "As early as 1975",
+				option4: "As early as 1990",
+			  answer: 1
+			},
+			{
+			  question: "What is the major route of elimination of guaiphenesin is?",
+			  option1: "Sweat",
+			  option2: "Saliva",
+			  option3: "Urine",
+			  option4: "Bile",
+			  answer: 3
+			}
+		  ]
+	];
