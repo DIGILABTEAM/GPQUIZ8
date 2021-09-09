611ffd5ad70e95e71d47dce22086fa8f96c82dd4
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-09-08 17:42:49 -0700

    Deploy Changes
    Deployed new changes in questions.js and Index.html

--- js/questions.js
+++ js/questions.js
@@ -3,44 +3,34 @@ var data =
 	[
 		[
 			{
-			  question: "21.	Which of the following is true statement?",
-			  option1: "Evidence has shown that Levosalbutamol is not cost-effective treatment when compared to racemic salbutamol",
-			  option2: "Evidence has shown that Levosalbutamol is more cost-effective treatment when compared to racemic salbutamol",
-			  option3: "Both are equally cost effective",
-			  option4: "None of the above",
-			  answer: 2
+				question: "Guaiphenesin is often regarded as wonder drug by opera singers",
+				option1: "TRUE",
+				option2: "FALSE",
+				answer: 1
 			},
 			{
-			  question: "What is the major route of excretion of levosalbutamol",
-			  option1: "Liver",
-			  option2: "Lungs",
-			  option3: "Kidneys",
-			  option4: "Sweat",
-			  answer: 3
+				question: "As a single agent, guaifenesin has a well-established and favorable safety and tolerability profile, supported by data from published clinical studies",
+				option1: "TRUE",
+				option2: "FALSE",
+				answer: 1
 			},
 			{
-			  question: "Which of the following statement is true regarding ambroxol?",
-			  option1: "There are many absolute contraindications for Ambroxol, but no caution is required in patients with gastric ulceration",
-			  option2: "Gastric ulceration is only absolute contraindication for ambroxol.",
-			  option3: "There are not many absolute contraindications for Ambroxol, but in patients with gastric ulceration, relative caution should be observed",
-			  option4: "None of the above",
-			  answer: 3
+				question: "The terminal half-life of ambroxol in the plasma is 7 - 12 hours",
+				option1: "TRUE",
+				option2: "FALSE",
+				answer: 1
 			},
 			{
-				question: "Guaiphenesin was first accepted by the USFDA",
-				option1: "As early as 1952",
-				option2: "As early as 1925",
-				option3: "As early as 1975",
-				option4: "As early as 1990",
-			  answer: 1
+				question: "In March 1999, Levosalbutamol was approved in the United States as a solution to be used with a nebulizer device",
+				option1: "True",
+				option2: "FALSE",
+				answer: 1
 			},
 			{
-			  question: "What is the major route of elimination of guaiphenesin is?",
-			  option1: "Sweat",
-			  option2: "Saliva",
-			  option3: "Urine",
-			  option4: "Bile",
-			  answer: 3
+				question: "30.	The use of Guaiphenesin as a natural remedy dates back to the 1500s, when guaiac tree extracts were used by Native Americans to treat various illnesses, like sore throat",
+				option1: "TRUE",
+				option2: "FALSE",
+				answer: 1
 			}
-		  ]
+		]
 	];
